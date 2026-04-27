a57e266ef51187a26fa44a70938da51bae3d44fd
San <sanwebdev@yandex.ru>
2026-04-27 21:05:55 +0300

    Improve main template formatting

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,8 @@
 <?php
+
 /** @var array $categories */
 /** @var array $lots */
+
 ?>
 <section class="promo">
     <h2 class="promo__title">Нужен стафф для катки?</h2>
@@ -24,11 +26,19 @@
         <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= esc($lot['image_url'] ?? '') ?>" width="350" height="260" alt="<?= esc($lot['title'] ?? '') ?>">
+                    <img
+                        src="<?= esc($lot['image_url'] ?? '') ?>"
+                        width="350"
+                        height="260"
+                        alt="<?= esc($lot['title'] ?? '') ?>"
+                    >
                 </div>
+
                 <div class="lot__info">
                     <span class="lot__category"><?= esc($lot['category'] ?? '') ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= esc($lot['title'] ?? '') ?></a></h3>
+                    <h3 class="lot__title">
+                        <a class="text-link" href="pages/lot.html"><?= esc($lot['title'] ?? '') ?></a>
+                    </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount"><?= format_price($lot['price'] ?? 0) ?></span>
