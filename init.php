1a8d61746a448c080d68042bba960b7dd30794e2
San <sanwebdev@yandex.ru>
2026-05-06 20:11:20 +0300

    Move helpers.php and functions.php to /functions

--- init.php
+++ init.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 date_default_timezone_set('Europe/Moscow');
 
 require_once __DIR__ . '/const.php';
-require_once __DIR__ . '/helpers.php';
-require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/functions/helpers.php';
+require_once __DIR__ . '/functions/functions.php';
