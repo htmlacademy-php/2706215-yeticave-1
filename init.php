e39e3c2a5461bcb67296ce0b7846fc1f0ec96c9b
San <sanwebdev@yandex.ru>
2026-05-06 20:20:18 +0300

    Move const.php to /util

--- init.php
+++ init.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 date_default_timezone_set('Europe/Moscow');
 
-require_once __DIR__ . '/const.php';
+require_once __DIR__ . '/util/const.php';
 require_once __DIR__ . '/functions/helpers.php';
 require_once __DIR__ . '/functions/functions.php';
